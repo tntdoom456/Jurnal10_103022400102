088eb684a4f47fcca4185e0719477435c0deb443
rifkialfatahCUPUmek <ilypussy@gmail.com>
2026-05-09 08:04:52 +0700

    code selesai

---
+++ Controllers/GameControllers.cs
@@ -0,0 +1,43 @@
+﻿using Jurnal10_103022400102.model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jurnal10_103022400102.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class GameControllers : ControllerBase
+    {
+        private static List<Game> games = new List<Game>
+        {
+            new Game(1, "The Legend of Zelda: Breath of the Wild", "Nintendo", 2017, "Action-adventure", 9.5, new string[] { "Nintendo Switch", "Wii U" }, new string[] { "Single-player" }, false, 59),
+            new Game(2, "Red Dead Redemption 2", "Rockstar Games", 2018, "Action-adventure", 9.8, new string[] { "PlayStation 4", "Xbox One", "PC" }, new string[] { "Single-player", "Multiplayer" }, true, 59),
+            new Game(3, "The Witcher 3: Wild Hunt", "CD Projekt Red", 2015, "Action role-playing", 9.7, new string[] { "PlayStation 4", "Xbox One", "PC" }, new string[] { "Single-player" }, false, 39),
+            new Game(4, "Minecraft", "Mojang Studios", 2011, "Sandbox, survival", 9.0, new string[] { "PC", "Console", "Mobile" }, new string[] { "Single-player", "Multiplayer" }, true, 26),
+            new Game(5, "Fortnite", "Epic Games", 2017, "Battle royale", 8.5, new string[] { "PC", "Console", "Mobile" }, new string[] { "Multiplayer" }, true, 0)
+        };
+
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            return games;
+        }
+
+        [HttpGet("{id}")]
+        public Game Get(int id)
+        {
+            return games[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Game game)
+        {
+            games.Add(game);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            games.RemoveAt(id);
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿namespace Jurnal10_103022400102.model
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public string developer { get; set; }
+        public int tahunRilis { get; set; }
+        public string genre { get; set; }
+        public double rating { get; set; }
+        public string[] platform { get; set; }
+        public string[] mode { get; set; }
+        public bool isOnline { get; set; }
+        public int harga { get; set; }
+
+        public Game(int id, string nama, string developer, int tahunRilis, string genre, double rating, string[] platform, string[] mode, bool isOnline, int harga)
+        {
+            this.id = id;
+            this.nama = nama;
+            this.developer = developer;
+            this.tahunRilis = tahunRilis;
+            this.genre = genre;
+            this.rating = rating;
+            this.platform = platform;
+            this.mode = mode;
+            this.isOnline = isOnline;
+            this.harga = harga;
+        }
+
+    }
+}
